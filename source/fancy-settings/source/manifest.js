3e7a02ebf3492c9da24a41ce9292023aeb5339ea
Alan Berndt <alan@eatabrick.org>
2011-08-15 14:05:26 -0700

    version 2.1

---
+++ source/fancy-settings/source/manifest.js
@@ -0,0 +1,22 @@
+this.manifest = {
+  "name": "eab.so shortener",
+  "icon": "../../icon.png",
+  "settings": [
+    {
+      "tab": "Main",
+      "group": "",
+      "name": "username",
+      "type": "text",
+      "label": "Post as",
+      "text": "Some asshole"
+    },
+    { 
+      "tab": "Main",
+      "group": "",
+      "name": "hidden",
+      "type": "checkbox",
+      "label": "Hide links from RSS feed"
+    } 
+  ]
+};
+
---
+++ source/popup.js
@@ -0,0 +1,21 @@
+$(document).ready( function() {
+  chrome.extension.sendRequest({action: 'getOpts'}, function(settings) {
+    chrome.tabs.getSelected(null, function(tab) {
+      var data = {
+        "uri": tab.url,
+        "user": settings.username,
+        "hide": settings.hidden
+      };
+
+      $.post('http://eab.so/new.pl', data, function(response) {
+        if (response.error) {
+          $('p').addClass('error');
+          $('p').text(response.error);
+        } else {
+          $('p').text(response.result);
+        }
+      }); 
+    });
+  });
+});
+
